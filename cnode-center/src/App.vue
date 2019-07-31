b5f3a0d91ba674de9e5c608c2008527f88b8c42d
mark <197100724@qq.com>
2019-07-31 21:34:10 +0800

    'cnode.js'

---
+++ cnode-center/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <headers></headers>
+    <div class="main">
+      <router-view name='slidebar'></router-view>
+      <router-view name='main'></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import headers from "./components/Header";
+import PostList from "./components/PostList";
+export default {
+  name: "App",
+  components: {
+    headers,
+    PostList
+  }
+};
+</script>
+
+<style>
+.main {
+  background: #e1e1e1;
+  padding: 4px 75px;
+}
+body{
+  margin: 0;
+  
+}
+</style>
